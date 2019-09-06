9f0a286ed401c1e5dafe8d24509c9c98c7e0f7bb
ltyjason <carzylty@163.com>
2019-09-06 23:00:59 +0800

    message

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from App.models import Goods, UserModel
+
+
+class GoodsAdmin(admin.ModelAdmin):
+    list_display = ['productlongname', 'price']
+    list_filter = ['childcidname']
+    search_fields = ['productlongname']
+    list_per_page = 20
+    ordering = ['price']
+    fieldsets = (
+        ('商品分类', {'fields': ('childcidname',)}),
+        ('基本信息', {'fields': ('productlongname', 'price',)}),
+    )
+
+
+admin.site.register(Goods, GoodsAdmin)
+
+admin.site.register(UserModel)
+
